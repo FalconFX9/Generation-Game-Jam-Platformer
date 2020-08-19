db7bda5778360f5f4adf5884f89df58f659bfe77
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2020-08-18 21:31:30 -0400

    Fixed the physics engine, moving platforms now properly work with vertical and horizontal momentum. Most levels seem to be possible.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import arcade
 import random
 import math
+import time
+import collections
 import constants as C
 from player import Player
 from physics_engine import PhysicsEngine
@@ -13,10 +15,11 @@ def procedural_generator() -> (arcade.SpriteList, arcade.SpriteList):
 
     seed = random.randint(0, 10000)
     print(seed)
+    # 2226
     rng = random.Random(seed)
     platform = arcade.Sprite('Platform.png')
     platform.center_x = 800
-    platform.center_y = rng.randint(800, 2000)
+    platform.center_y = rng.randint(1500, 2500)
     platform.color = arcade.color.WHITE
     platforms.append(platform)
     current_x = 800
@@ -26,16 +29,15 @@ def procedural_generator() -> (arcade.SpriteList, arcade.SpriteList):
     while current_y > 85:
         platform = arcade.Sprite('Platform.png')
         x_offset = rng.randint(30, 210)
-        if rng.random() < 0.2:
-            if rng.random() < 1:
+        if rng.random() < 0.1 and (not platforms[-1].change_x or not platforms[-1].change_y):
+            if rng.random() < 0.5:
                 x_offset = rng.randint(300, 550)
                 platforms[-1].boundary_left = platforms[-1].center_x
                 platforms[-1].boundary_right = current_x + x_offset
                 platforms[-1].change_x = 3
                 current_x = platforms[-1].boundary_right
-            if rng.random() < 1:
+            if rng.random() < 0.5:
                 generated_y = rng.randint(current_y - 300, current_y - 150)
-                platforms[-1].center_y += 50
                 platforms[-1].boundary_top = platforms[-1].center_y
                 platforms[-1].boundary_bottom = generated_y
                 platforms[-1].change_y = -3
@@ -48,7 +50,7 @@ def procedural_generator() -> (arcade.SpriteList, arcade.SpriteList):
                 platforms[-1].change_y = -(3 / ratio)
                 print(x_distance, y_distance, ratio, x_distance/3, y_distance/(3/ratio))
         else:
-            generated_y = rng.randint(current_y - 90, current_y - 50)
+            generated_y = rng.randint(current_y - 90, current_y - 58)
             if platforms[-1].change_y:
                 generated_y += 20
             generated_x = current_x - rng.choice((x_offset, -x_offset))
@@ -58,14 +60,33 @@ def procedural_generator() -> (arcade.SpriteList, arcade.SpriteList):
             platforms.append(platform)
             current_y = platform.center_y
             current_x = platform.center_x
-            if rng.random() < 0.05:
-                ennemy = arcade.Sprite(":resources:images/space_shooter/playerShip1_green.png", 0.3)
-                ennemy.center_x = platform.center_x
-                ennemy.center_y = platform.center_y + 50
-                ennemy.angle = 90
-                ennemies.append(ennemy)
+            #if rng.random() < 0.05:
+            #    ennemy = arcade.Sprite(":resources:images/space_shooter/playerShip1_green.png", 0.3)
+            #    ennemy.center_x = platform.center_x
+            #    ennemy.center_y = platform.center_y + 50
+            #    ennemy.angle = 90
+            #    ennemies.append(ennemy)
 
     return platforms, ennemies
+
+
+class FPSCounter:
+    def __init__(self):
+        self.time = time.perf_counter()
+        self.frame_times = collections.deque(maxlen=60)
+
+    def tick(self):
+        t1 = time.perf_counter()
+        dt = t1 - self.time
+        self.time = t1
+        self.frame_times.append(dt)
+
+    def get_fps(self):
+        total_time = sum(self.frame_times)
+        if total_time == 0:
+            return 0
+        else:
+            return len(self.frame_times) / sum(self.frame_times)
 
 
 class Game(arcade.View):
@@ -82,6 +103,7 @@ class Game(arcade.View):
         self.w_pressed = False
         self.a_pressed = False
         self.d_pressed = False
+        self.jump_needs_reset = False
 
         self.mouse_press = False
 
@@ -91,6 +113,7 @@ class Game(arcade.View):
 
         self.view_bottom = 0
         self.view_left = 0
+        self.fps = FPSCounter()
 
         self.window.background_color = arcade.color.BLACK
 
@@ -118,29 +141,32 @@ class Game(arcade.View):
             arcade.draw_arc_outline(800 + self.view_left, 450 + self.view_bottom, 300, 300, arcade.color.YELLOW, 0, 180, 15)
             arcade.draw_arc_outline(800 + self.view_left, 450 + self.view_bottom, 300, 300, arcade.color.BLUE, 180, 360, 15)
 
+        fps = self.fps.get_fps()
+        output = f"FPS: {fps:3.0f}"
+        arcade.draw_text(output, 20 + self.view_left, C.SCREEN_HEIGHT - 80, arcade.color.WHITE, 16)
+
     def on_update(self, delta_time: float):
         self.frame_count += 1
         self.player_sprite.current_background = self.window.background_color
 
         if self.w_pressed:
-            if self.physics_engine.can_jump():
-                self.physics_engine.jump(10)
+            jump_test = self.physics_engine.can_jump()
+            if jump_test[0] and not self.jump_needs_reset:
+                self.physics_engine.jump(10 + jump_test[1])
+                self.jump_needs_reset = True
         if self.a_pressed and not self.d_pressed:
-            if self.player_sprite.change_x > - C.MAX_SPEED + self.player_sprite.momentum:
+            if self.player_sprite.change_x > - (C.MAX_SPEED + self.player_sprite.momentum):
                 self.player_sprite.change_x -= 0.1 * self.player_sprite.speed_multiplier
         elif self.d_pressed and not self.a_pressed:
             if self.player_sprite.change_x < C.MAX_SPEED + self.player_sprite.momentum:
                 self.player_sprite.change_x += 0.1 * self.player_sprite.speed_multiplier
         else:
-            if self.player_sprite.change_x > C.FRICTION * self.player_sprite.speed_multiplier + self.player_sprite.momentum:
+            if self.player_sprite.change_x > C.FRICTION * self.player_sprite.speed_multiplier:
                 self.player_sprite.change_x -= C.FRICTION * self.player_sprite.speed_multiplier
-            elif self.player_sprite.change_x < - C.FRICTION * self.player_sprite.speed_multiplier + self.player_sprite.momentum:
+            elif self.player_sprite.change_x < - C.FRICTION * self.player_sprite.speed_multiplier:
                 self.player_sprite.change_x += C.FRICTION * self.player_sprite.speed_multiplier
             else:
-                if self.player_sprite.momentum != 0:
-                    self.player_sprite.change_x = self.player_sprite.momentum
-                else:
-                    self.player_sprite.change_x = 0
+                self.player_sprite.change_x = 0
 
         self.physics_engine.update()
 
@@ -158,17 +184,17 @@ class Game(arcade.View):
 
                 enemy.angle = math.degrees(angle) - 90
 
-                if self.frame_count % 30 == 0:
-                    bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", 0.5)
-                    bullet.center_x = start_x
-                    bullet.center_y = start_y
+                #if self.frame_count % 30 == 0:
+                #    bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", 0.5)
+                #    bullet.center_x = start_x
+                #    bullet.center_y = start_y
 
-                    bullet.angle = math.degrees(angle)
+                #    bullet.angle = math.degrees(angle)
 
-                    bullet.change_x = math.cos(angle) * C.BULLET_SPEED
-                    bullet.change_y = math.sin(angle) * C.BULLET_SPEED
+                #    bullet.change_x = math.cos(angle) * C.BULLET_SPEED
+                #    bullet.change_y = math.sin(angle) * C.BULLET_SPEED
 
-                    self.bullet_list.append(bullet)
+                #    self.bullet_list.append(bullet)
 
         for bullet in self.bullet_list:
             if self.view_left > bullet.center_x or bullet.center_x > C.SCREEN_WIDTH + self.view_left or \
@@ -177,20 +203,6 @@ class Game(arcade.View):
 
         self.bullet_list.update()
         changed = False
-
-        self.platforms.update()
-
-        # See if the wall hit a boundary and needs to reverse direction.
-        for platform in self.platforms:
-
-            if platform.boundary_right and platform.center_x > platform.boundary_right and platform.change_x > 0:
-                platform.change_x *= -1
-            if platform.boundary_left and platform.center_x < platform.boundary_left and platform.change_x < 0:
-                platform.change_x *= -1
-            if platform.boundary_bottom and platform.center_y < platform.boundary_bottom and platform.change_y < 0:
-                platform.change_y *= -1
-            if platform.boundary_top and platform.center_y > platform.boundary_top and platform.change_y > 0:
-                platform.change_y *= -1
 
         # Scroll left
         left_boundary = self.view_left + C.LEFT_VIEWPORT_MARGIN
@@ -225,8 +237,9 @@ class Game(arcade.View):
                                 C.SCREEN_HEIGHT + self.view_bottom)
 
         if self.player_sprite.top >= self.platforms[0].center_y + 95:
-            print(self.player_sprite.top)
             self.setup()
+
+        self.fps.tick()
 
     def on_key_press(self, symbol: int, modifiers: int):
         if symbol == arcade.key.W:
@@ -235,10 +248,13 @@ class Game(arcade.View):
             self.a_pressed = True
         elif symbol == arcade.key.D:
             self.d_pressed = True
+        elif symbol == arcade.key.R:
+            self.setup()
 
     def on_key_release(self, symbol: int, modifiers: int):
         if symbol == arcade.key.W:
             self.w_pressed = False
+            self.jump_needs_reset = False
         elif symbol == arcade.key.A:
             self.a_pressed = False
         elif symbol == arcade.key.D:
--- physics_engine.py
+++ physics_engine.py
@@ -15,34 +15,37 @@ def _move_sprite(moving_sprite: arcade.Sprite, platforms: arcade.SpriteList):
     for platform in hit_list_y:
         if platform.color != moving_sprite.current_background:
             if moving_sprite.change_y > 0:
-                moving_sprite.top = platform.bottom
+                moving_sprite.top = platform.bottom - 5
+                if platform.change_y < 0:
+                    moving_sprite.change_y = platform.change_y
+                    moving_sprite.center_y += platform.change_y
+                else:
+                    moving_sprite.change_y = 0
             elif moving_sprite.change_y < 0:
                 moving_sprite.bottom = platform.top
+                if platform.change_y > 0:
+                    moving_sprite.change_y = 0
+                else:
+                    moving_sprite.change_y = -3
+                if platform.change_x:
+                    moving_sprite.momentum = platform.change_x
 
-            if platform.change_x:
-                if moving_sprite.momentum == 0:
-                    moving_sprite.change_x += platform.change_x
-                elif moving_sprite.momentum == platform.change_x * -1:
-                    moving_sprite.change_x += platform.change_x * 2
-                moving_sprite.momentum = platform.change_x
-
-            moving_sprite.change_y = 0
-
-    if len(hit_list_y) == 0:
-        moving_sprite.momentum = 0
-
-    moving_sprite.center_x += moving_sprite.change_x * moving_sprite.speed_multiplier
+    moving_sprite.center_x += (moving_sprite.change_x + moving_sprite.momentum) * moving_sprite.speed_multiplier
 
     hit_list_x = arcade.check_for_collision_with_list(moving_sprite, platforms)
 
     for platform in hit_list_x:
-        if platform.color != moving_sprite.current_background:
+        if platform.color != moving_sprite.current_background and platform not in hit_list_y:
             if moving_sprite.change_x > 0:
                 moving_sprite.right = platform.left
             elif moving_sprite.change_x < 0:
                 moving_sprite.left = platform.right
 
             moving_sprite.change_x = 0
+
+    if len(hit_list_y) <= 0 and moving_sprite.momentum != 0:
+        moving_sprite.change_x += moving_sprite.momentum
+        moving_sprite.momentum = 0
 
 
 class PhysicsEngine:
@@ -62,7 +65,7 @@ class PhysicsEngine:
             else:
                 return False
 
-    def can_jump(self, y_distance=5) -> bool:
+    def can_jump(self, y_distance=5) -> (bool, float):
         self.player.center_y -= y_distance
 
         hit_list = arcade.check_for_collision_with_list(self.player, self.platforms)
@@ -70,13 +73,15 @@ class PhysicsEngine:
         self.player.center_y += y_distance
 
         if (len(hit_list) > 0 and hit_list[0].color != self.player.current_background) or self.player.bottom <= 0:
-            return True
+            if self.player.bottom <= 0:
+                return True, 0
+            else:
+                return True, hit_list[0].change_y
         else:
-            return False
+            return False, 0
 
     def jump(self, velocity: int):
         self.player.change_y = velocity
-        self.player.momentum = 0
 
     def can_switch(self):
         hit_list = arcade.check_for_collision_with_list(self.player, self.platforms)
@@ -91,3 +96,46 @@ class PhysicsEngine:
             self.player.change_y -= self.gravity_constant * self.player.speed_multiplier
 
         _move_sprite(self.player, self.platforms)
+
+        for platform in self.platforms:
+            if platform.change_x != 0 or platform.change_y != 0:
+                platform.center_x += platform.change_x * self.player.speed_multiplier
+
+                if platform.boundary_left is not None \
+                        and platform.center_x <= platform.boundary_left:
+                    platform.center_x = platform.boundary_left
+                    if platform.change_x < 0:
+                        platform.change_x *= -1
+
+                if platform.boundary_right is not None \
+                        and platform.center_x >= platform.boundary_right:
+                    platform.center_x = platform.boundary_right
+                    if platform.change_x > 0:
+                        platform.change_x *= -1
+
+                collision = arcade.check_for_collision(self.player, platform)
+                if collision and not platform.change_y:
+                    if platform.change_x < 0:
+                        self.player.right = platform.left
+                    if platform.change_x > 0:
+                        self.player.left = platform.right
+
+                platform.center_y += platform.change_y * self.player.speed_multiplier
+
+                if platform.boundary_top is not None \
+                        and platform.center_y >= platform.boundary_top:
+                    platform.center_y = platform.boundary_top
+                    if platform.change_y > 0:
+                        platform.change_y *= -1
+
+                if platform.boundary_bottom is not None \
+                        and platform.center_y <= platform.boundary_bottom:
+                    platform.center_y = platform.boundary_bottom
+                    if platform.change_y < 0:
+                        platform.change_y *= -1
+
+                collision = arcade.check_for_collision(self.player, platform)
+                if collision:
+                    if platform.change_y < 0 and self.player.change_y > 0:
+                        self.player.top = platform.bottom
+                        self.player.change_y = -3
